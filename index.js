e6e986926934178d6d2a113a1d91d1681ebd8070
DANKAMN <danielnicholas1477@gmail.com>
2023-11-20 14:12:11 +0100

    validation

--- index.js
+++ index.js
@@ -53,25 +53,6 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-
-//Create a note
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
 //Get specific note
 app.get('/api/notes/:id', (request, response) => {
   Note.findById(request.params.id).then(note => {
@@ -86,16 +67,27 @@ app.get('/api/notes/:id', (request, response) => {
   })
 })
 
-//Update the importance of a note
-app.put('/api/notes/:id', (request, response, next) => {
+//Create a note
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important,
-  }
+    important: body.important || false,
+  })
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
+})
+
+//Update the importance of a note
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(request.params.id, { content, important },
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -119,6 +111,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     response.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
